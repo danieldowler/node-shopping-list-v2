9085cb7a5124e42fed565b86ffacd397a049f5b5
Daniel Dowler <shaolinsamurai1988@gmail.com>
2018-05-07 16:50:09 -0600

    working POST udpate

--- server.js
+++ server.js
@@ -43,7 +43,19 @@ app.post('/shopping-list', jsonParser, (req, res) => {
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
+  app.post('/recipies', jsonParser, (req, res) => {
+    // ensure `name` and `budget` are in request body
+    const requiredFields = ['name', 'ingredients'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+
+  const item = Recipies.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
 
